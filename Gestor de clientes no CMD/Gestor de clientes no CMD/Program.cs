4d4ab47f9efd9ccd79a2dfec504f176ad13a9657
Davi Silva Nascimento <davisilva_nascimento@hotmail.com>
2020-12-23 21:37:13 -0300

    Menu criado

--- Gestor de clientes no CMD/Gestor de clientes no CMD/Program.cs
+++ Gestor de clientes no CMD/Gestor de clientes no CMD/Program.cs
@@ -8,8 +8,34 @@ namespace Gestor_de_clientes_no_CMD
 {
     class Program
     {
+        enum Menu //Opções do menu do usuario
+        {
+            Listagem = 1, Adicionar, Remover, Sair
+        }
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Sistema de gerenciamento de cliente - Digite sua opção:");
+            for (bool op = false; op == false;)
+            {
+                Console.WriteLine("||Digite sua opção:||");
+                Console.WriteLine("||1-Lista de clientes\n||2-Adicionar um cliente\n||3-Remover um cliente\n||4-Sair do programa");
+                Menu opcao = (Menu)int.Parse(Console.ReadLine());
+                switch (opcao)
+                {
+                    case Menu.Adicionar:
+                        break;
+                    case Menu.Listagem:
+                        break;
+                    case Menu.Remover:
+                        break;
+                    case Menu.Sair:
+                        op = true;
+                        break;
+                }
+                Console.Clear();
+            }
+
         }
     }
 }
